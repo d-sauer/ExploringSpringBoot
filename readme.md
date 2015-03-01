8f57151818b6ffe6020c8eb960a4928574dda299
Davor Sauer <davor@backbase.com>
2015-03-01 14:50:11 +0100

    Gradle build FatJar

--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 #Exploring Spring Boot
 
 https://spring.io/guides/gs/spring-boot/
+
+
+Run project: gradle bootRun
--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -1,9 +1,12 @@
 package hello;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import java.util.Date;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableAutoConfiguration
@@ -12,7 +15,8 @@ public class SampleController {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World!";
+        Date date = new Date();
+        return "Hello Docker on " + date;
     }
 
     public static void main(String[] args) throws Exception {
